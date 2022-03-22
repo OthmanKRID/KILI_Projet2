31632543173b3e19a9e32b674c2d14d6692b2910
brunoprovenier <bruno.provenier@gmail.com>
2022-03-22 10:37:49 +0100

    Ajout des roles userAccount

--- Kili/Models/General/UserAccount.cs
+++ Kili/Models/General/UserAccount.cs
@@ -6,7 +6,7 @@ namespace Kili.Models.General
 {
     public class UserAccount
     {
-        public int  Id { get; set; }
+        public int Id { get; set; }
         [MaxLength(25)]
         public string UserName { get; set; }
 
@@ -14,13 +14,24 @@ namespace Kili.Models.General
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,}$", ErrorMessage = "Votre mot de passe doit avoir au moins 8 caractères dont une lettre en majuscule, un nombre et un caractère spécial")]
         public string Password { get; set; }
 
+        [Column(TypeName = "date")]
         public DateTime DateCreation { get; set; }
         public bool Actif { get; set; }
-        
+
         [EmailAddress(ErrorMessage = "L'adresse email saisie n'est pas valide")]
         public string Mail { get; set; }
         public string URLPhoto { get; set; }
         public int? PersonneID { get; set; }
         public virtual Personne Personne { get; set; }
+
+        public TypeRole Role { get; set; }
+
+    }
+
+    public enum TypeRole
+    {
+        Admin,
+        Association,
+        Utilisateur
     }
 }
