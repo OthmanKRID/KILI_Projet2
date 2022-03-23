e16fc0563317a13b0f2bb50ebeb55745fb825a7b
antoinemotte5 <97730024+antoinemotte5@users.noreply.github.com>
2022-03-23 08:48:13 +0100

    Début de dons

---
+++ Kili/Models/Dons/Donateur.cs
@@ -0,0 +1,18 @@
+﻿using Kili.Models.General;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kili.Models.Dons
+{
+    public class Donateur
+    {
+        public int Id { get; set; }
+        public Adresse AdresseFacuration { get; set; }
+        [MaxLength(10)]
+        public string Telephone { get; set; }
+        //[EmailAddress(ErrorMessage = "L'adresse email saisie n'est pas valide")]
+        //public string Mail { get; set; }
+        public int? UserAccountId { get; set; }
+        public virtual UserAccount UserAccount { get; set; }
+
+    }
+}
